4273752a3109093ecd62ca6aa6d8665eff02430c
LucasAndNay <lucas-kalache@hotmail.com>
2015-08-13 11:18:22 +0000

    primeira versão (nova) até a 2 C

--- app/src/main/java/com/example/xubuntu_developer/testando/AtividadePrincipalNova.java
+++ app/src/main/java/com/example/xubuntu_developer/testando/AtividadePrincipalNova.java
@@ -1,17 +1,47 @@
 package com.example.xubuntu_developer.testando;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class AtividadePrincipalNova extends ActionBarActivity {
+    private ArrayAdapter mAdaptador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_atividade_principal_nova);
+        String[] dados = new String[]{
+                "NossaSala",
+
+                "Angela",
+                "Nayara",
+                "Éder",
+                "Lucas",
+                "Ricardo",
+                "fátima"
+        };
+
+        List<String> NossaSala = new ArrayList<>(Arrays.asList(dados));
+
+
+        mAdaptador = new ArrayAdapter<>(
+                getApplicationContext(), // Contexto atual
+                R.layout.item_lista_principal, // Nome do ID do layout
+                R.id.item_texto, // ID do TextView a ser preenchido
+                NossaSala);
+
+        ListView listView = (ListView) findViewById(R.id.lista_principal);
+        listView.setAdapter(mAdaptador);
+
+
     }
 
     @Override
